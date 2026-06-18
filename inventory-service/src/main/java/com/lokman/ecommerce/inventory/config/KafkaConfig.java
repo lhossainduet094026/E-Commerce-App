64a0483159b0d75086923236839a21993db3c445
lhossainduet094026 <lhossainduet094026@gmail.com>
2026-06-18 19:10:40 +0600

    feat(KafkaConfig) - KafkaConfig added for handling JSONDeserialization error.

---
+++ inventory-service/src/main/java/com/lokman/ecommerce/inventory/config/KafkaConfig.java
@@ -0,0 +1,37 @@
+package com.lokman.ecommerce.inventory.config;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.serializer.DeserializationException;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+	@Bean
+	public DeadLetterPublishingRecoverer recoverer(KafkaTemplate<String, Object> template) {
+//		// Now we create the recoverer and give it the rule book
+		return new DeadLetterPublishingRecoverer(template, this::resolveDLQDestination);
+	}
+
+	private TopicPartition resolveDLQDestination(ConsumerRecord<?, ?> record, Exception exception) {
+		String dlqTopic = record.topic() + ".DLT";
+		return new TopicPartition(dlqTopic, record.partition());
+	}
+
+	@Bean
+	public DefaultErrorHandler handler(DeadLetterPublishingRecoverer recoverer) {
+
+		DefaultErrorHandler handler = new DefaultErrorHandler(recoverer);
+
+		handler.addNotRetryableExceptions(DeserializationException.class);
+
+		return handler;
+	}
+}
