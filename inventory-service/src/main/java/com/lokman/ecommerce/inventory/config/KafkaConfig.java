0197ed69b9719324674b9a0e0321df09bef93440
lhossainduet094026 <lhossainduet094026@gmail.com>
2026-06-19 18:57:56 +0600

    feat(kafka): configure Kafka listener container factory for consumer concurrency and error handling

--- inventory-service/src/main/java/com/lokman/ecommerce/inventory/config/KafkaConfig.java
+++ inventory-service/src/main/java/com/lokman/ecommerce/inventory/config/KafkaConfig.java
@@ -5,10 +5,15 @@ import org.apache.kafka.common.TopicPartition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.DeserializationException;
+
+import com.lokman.ecommerce.inventory.kafka.event.OrderCreatedEvent;
 
 @Configuration
 @EnableKafka
@@ -33,5 +38,17 @@ public class KafkaConfig {
 		handler.addNotRetryableExceptions(DeserializationException.class);
 
 		return handler;
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent> kafkaListenerContainerFactory(
+			DefaultErrorHandler errorHandler, 
+			ConsumerFactory<String, OrderCreatedEvent> cf) {
+		
+		var factory = new ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent>();
+		factory.setConsumerFactory(cf);
+		factory.setCommonErrorHandler(errorHandler);
+		factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+		return factory;
 	}
 }
